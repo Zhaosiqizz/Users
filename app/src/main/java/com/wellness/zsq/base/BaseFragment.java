4a477d08f5f59cee74173635ea29bcbf84372d86
zsq.wellness <zsqandzyr@hotmail.com>
2018-02-26 16:25:45 +0800

    项目搭建

--- app/src/main/java/com/wellness/zsq/base/BaseFragment.java
+++ app/src/main/java/com/wellness/zsq/base/BaseFragment.java
@@ -1,8 +1,36 @@
 package com.wellness.zsq.base;
 
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
 /**
  * Created by wellness on 2018/2/26.
  */
 
-public class BaseFragment {
+public abstract class BaseFragment extends Fragment{
+    public abstract int getContentViewId();
+    protected Context context;
+    protected View mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        mRootView =inflater.inflate(getContentViewId(),container,false);
+        ButterKnife.bind(this,mRootView);
+        this.context = getActivity();
+        return mRootView;
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 }
--- app/src/main/java/com/wellness/zsq/login/RegisterActivity.java
+++ app/src/main/java/com/wellness/zsq/login/RegisterActivity.java
@@ -1,8 +1,44 @@
 package com.wellness.zsq.login;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.wellness.zsq.base.BaseActivity;
+import com.wellness.zsq.users.R;
+
 /**
  * Created by wellness on 2018/2/26.
  */
 
-public class RegisterActivity {
+public class RegisterActivity extends BaseActivity{
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_register;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setEvent() {
+
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
